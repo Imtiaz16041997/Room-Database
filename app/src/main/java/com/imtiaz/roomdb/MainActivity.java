84d2163e5289ea4ec141ab9f5210312af1da32c4
Imtiaz16041997 <imtiazeemel42@gmail.com>
2022-01-03 22:31:20 +0600

    modified existed data won't inserted

--- app/src/main/java/com/imtiaz/roomdb/MainActivity.java
+++ app/src/main/java/com/imtiaz/roomdb/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.imtiaz.roomdb.DAO.UserDao;
 import com.imtiaz.roomdb.Database.AppDatabase;
@@ -14,7 +15,7 @@ import com.imtiaz.roomdb.EntityModelClass.User;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText first_name,last_name;
+    EditText first_name,last_name,first_id;
     Button insertBtn;
     AppDatabase db;
     @Override
@@ -22,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        first_id = findViewById(R.id.first_id);
         first_name = findViewById(R.id.first_name);
         last_name = findViewById(R.id.last_name);
         insertBtn = findViewById(R.id.insertBtn);
@@ -31,9 +33,25 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 db = AppDatabase.getInstance(MainActivity.this);
                 UserDao userDao = db.userDao();
-                userDao.insertrecord(new User(1,first_name.getText().toString(),last_name.getText().toString()));
-                first_name.setText("");
-                last_name.setText("");
+
+                Boolean check = userDao.is_exist(Integer.parseInt(first_id.getText().toString()));
+                if(check == false){
+                    userDao.insertrecord(new User(Integer.parseInt(first_id.getText().toString()),first_name.getText().toString(),last_name.getText().toString()));
+                    first_id.setText("");
+                    first_name.setText("");
+                    last_name.setText("");
+                    Toast.makeText(MainActivity.this, "Inserted Successfully", Toast.LENGTH_SHORT).show();
+                }else {
+                    first_id.setText("");
+                    last_name.setText("");
+                    first_name.setText("");
+                    Toast.makeText(MainActivity.this, "Already Existed", Toast.LENGTH_SHORT).show();
+
+                }
+//                userDao.insertrecord(new User(1,first_name.getText().toString(), UserDao userDao = db.userDao();
+////            userDao.insertrecord(new User(1,first_name.getText().toString(),last_name.getText().toString()));
+////            first_name.setText("");
+
             }
         });
 
